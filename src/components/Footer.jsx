a1b0b55b8011ff7bad52b57dbbad1181baf490c6
Jim <merajuljim@gmai.com>
2025-12-15 14:20:59 +0600

    logo and footer edited

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,55 +5,53 @@ import useAuth from "../hooks/useAuth";
 import { FaFacebook } from "react-icons/fa";
 import { LiaLinkedin } from "react-icons/lia";
 import { BiX } from "react-icons/bi";
+import Logo from "./Logo";
 const Footer = () => {
   const { user } = useAuth();
 
   return (
     <div
-      className="bg-cover bg-no-repeat space-y-5 bg-center p-30 mt-5 md:mt-10 text-white font-bold flex flex-col-reverse md:flex-row justify-around"
+      className="bg-cover bg-no-repeat space-y-5 bg-center px-30 pt-10 mt-5 md:mt-10 text-white font-bold"
       style={{
         backgroundImage: `url(${footerBg})`,
       }}
     >
-      <div>
-        <h5 className="text-xl">Useful Links</h5>
-        <ul className="space-y-1">
-          <Link to="/">
-            <li>Home</li>
-          </Link>
-          <Link to="/donation-requests">
-            <li>Donation Requests</li>
-          </Link>
-
-          {user && (
-            <Link to="/donation-requests">
-              <li>Donate Us</li>
+      <div className="flex flex-col-reverse md:flex-row justify-around">
+        <div>
+          <h5 className="text-xl">Useful Links</h5>
+          <ul className="space-y-1">
+            <Link to="/">
+              <li>Home</li>
             </Link>
-          )}
-        </ul>
-      </div>
-      <div>
-        <h5 className="text-xl">Find Us On</h5>
-        <ul className="space-y-1">
-          <li>
-            <FaFacebook size={30}></FaFacebook>
-          </li>
-          <li>
-            <LiaLinkedin size={30}></LiaLinkedin>
-          </li>
-          <li>
-            <BiX size={30}></BiX>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <div className="w-15 md:w-20 lg:w-30 h-15 md:h-20 lg:h-30 mx-auto">
-          <img src="/src/assets/blood_donation_icon.jpg" alt="logo" />{" "}
+            <Link to="/donation-requests">
+              <li>Donation Requests</li>
+            </Link>
+
+            {user && (
+              <Link to="/donation-requests">
+                <li>Donate Us</li>
+              </Link>
+            )}
+          </ul>
         </div>
-        <h5 className="text-center text-xl md:text-2xl lg:text-4xl">
-          Red Help
-        </h5>
+        <div>
+          <h5 className="text-xl">Find Us On</h5>
+          <ul className="space-y-1">
+            <li>
+              <FaFacebook size={30}></FaFacebook>
+            </li>
+            <li>
+              <LiaLinkedin size={30}></LiaLinkedin>
+            </li>
+            <li>
+              <BiX size={30}></BiX>
+            </li>
+          </ul>
+        </div>
+        <Logo></Logo>
       </div>
+      <div className="border-b border-white w-11/12 mx-auto my-2"></div>
+      <p className="text-xs text-center">&copy;2025 Red Help</p>
     </div>
   );
 };
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,12 +3,13 @@ import { Link } from "react-router";
 
 const Logo = () => {
   return (
-    <Link
-      title="Red Help"
-      className="bg-white rounded-full p-0.5 w-8 md:w-15 h-8 md:h-15"
-      to="/"
-    >
-      <img src="/src/assets/blood_donation_icon.jpg" alt="logo" />
+    <Link title="Red Help" className="text-white font-bold" to="/">
+      <img
+        className="bg-white rounded-full p-0.5 w-8 md:w-15 h-8 md:h-15"
+        src="/src/assets/blood_donation_icon.jpg"
+        alt="logo"
+      />
+      <h4>Red Help</h4>
     </Link>
   );
 };
